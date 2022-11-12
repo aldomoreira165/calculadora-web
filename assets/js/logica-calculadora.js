2b8653e6f077841464328375746d7e6b9f351003
Aldo Vasquez <aldovasquez2014@gmail.com>
2022-11-11 22:11:29 -0600

    Finalizando estilos

---
+++ assets/js/logica-calculadora.js
@@ -0,0 +1,52 @@
+var pantalla = document.getElementById('pantalla-calculadora')
+var numero = ''
+
+document.getElementById('btn-0').addEventListener('click',  function(event){
+    numero += '0'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-1').addEventListener('click',  function(event){
+    numero += '1'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-2').addEventListener('click',  function(event){
+    numero += '2'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-3').addEventListener('click',  function(event){
+    numero += '3'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-4').addEventListener('click',  function(event){
+    numero += '4'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-5').addEventListener('click',  function(event){
+    numero += '5'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-6').addEventListener('click',  function(event){
+    numero += '6'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-7').addEventListener('click',  function(event){
+    numero += '7'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-8').addEventListener('click',  function(event){
+    numero += '8'
+    pantalla.innerHTML = numero
+})
+
+document.getElementById('btn-9').addEventListener('click',  function(event){
+    numero += '9'
+    pantalla.innerHTML = numero
+})
